fe8379bcd793bd97cffe429d1dbbc7a5f289e3fb
Yeimer Pinedo <yjpinedo@unicesar.edu.co>
2022-06-20 12:38:33 -0500

    Components livewire Category, Post, Tag

---
+++ app/Http/Livewire/Dashboard/Category/Save.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Dashboard\Category;
+
+use Livewire\Component;
+
+class Save extends Component
+{
+    public function render()
+    {
+        return view('livewire.dashboard.category.save');
+    }
+}
