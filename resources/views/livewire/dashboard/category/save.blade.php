8e765a833407d558c4e92cf27cc62a7bf6e96f79
Yeimer Pinedo <yjpinedo@unicesar.edu.co>
2022-09-17 21:52:07 -0500

    Styles form

--- resources/views/livewire/dashboard/category/save.blade.php
+++ resources/views/livewire/dashboard/category/save.blade.php
@@ -1,23 +1,42 @@
-<div>
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <x-jet-form-section submit='submit'>
 
-    <x-jet-action-message on="created">
-        {{ __('Saved.') }}
-    </x-jet-action-message>
+            <x-slot name="title">
+                {{ __('Categories') }}
+            </x-slot>
 
-    <form wire:submit.prevent='submit'>
-        <x-jet-input type="text" wire:model='title' placeholder="Title" />
-        <x-jet-input-error for="title" />
+            <x-slot name="description">
+                {{ __('Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam reiciendis obcaecati ratione ad, maxime provident velit eveniet repellat, quam suscipit placeat voluptate. Obcaecati necessitatibus suscipit deserunt, impedit cupiditate ut facilis.') }}
+            </x-slot>
 
-        <x-jet-input type="text" wire:model='text' placeholder="Text" />
-        <x-jet-input-error for="text" />
+            <x-slot name="form">
+                <div class="col-span-6 sm:col-span-4">
+                    <x-jet-label for="name" value="{{ __('Title') }}" />
+                    <x-jet-input type="text" wire:model='title' placeholder="Title" class="mt-1 block w-full" />
+                    <x-jet-input-error for="title" />
+                </div>
 
-        <x-jet-input type="file" wire:model='image' />
-        <x-jet-input-error for="image" />
+                <div class="col-span-6 sm:col-span-4">
+                    <x-jet-label for="name" value="{{ __('Text') }}" />
+                    <x-jet-input type="text" wire:model='text' placeholder="Text" class="mt-1 block w-full" />
+                    <x-jet-input-error for="text" />
+                </div>
 
-        @if ($category && $category->image)
-            <img class="w-40 my-3" src="{{ $category->getImageURL() }}" alt="">
-        @endif
+                <div class="col-span-6 sm:col-span-4">
+                    <x-jet-label for="name" value="{{ __('Image') }}" />
+                    <x-jet-input type="file" wire:model='image' class="mt-1 block w-full" />
+                    <x-jet-input-error for="image" />
+                    @if ($category && $category->image)
+                        <img class="w-40 my-3" src="{{ $category->getImageURL() }}" alt="">
+                    @endif
+                </div>
 
-        <x-jet-button type="submit">Enviar</x-jet-button>
-    </form>
+                <x-slot name="actions">
+                    <x-jet-action-message on="created">{{ __('Saved.') }}</x-jet-action-message>
+                    <x-jet-button type="submit">{{ __('Saved.') }}</x-jet-button>
+                </x-slot>
+            </x-slot>
+        </x-jet-form-section>
+    </div>
 </div>
