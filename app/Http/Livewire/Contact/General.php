b59c7905cca764c6f396391860e17689eef46d01
Yeimer Pinedo <yjpinedo@unicesar.edu.co>
2022-09-26 10:01:51 -0500

    Add migration and components livewire

---
+++ app/Http/Livewire/Contact/General.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Contact;
+
+use Livewire\Component;
+
+class General extends Component
+{
+    public function render()
+    {
+        return view('livewire.contact.general');
+    }
+}
---
+++ database/migrations/2022_09_26_141851_create_contact_people_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contact_people', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->string('surname', 80);
+            $table->string('other', 255)->nullable();
+            $table->enum('choices', ['adverd', 'post', 'course', 'movie', 'other']);
+            $table->foreignId('contact_general_id')->constrained()->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contact_people');
+    }
+};
